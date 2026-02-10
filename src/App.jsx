ab69931ebaf772a1a7d30c6e1b2fa6386ba9f829
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-10 13:30:33 +0100

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+const headLines = [
+  "Come organizzare la tua giornata in modo efficace",
+  "5 strumenti digitali indispensabili per lavorare meglio",
+  "Guida base al minimalismo digitale",
+  "Come migliorare la concentrazione in ambienti rumorosi",
+  "Introduzione al lavoro da remoto",
+  "Perché le pause migliorano la produttività",
+  "Le basi della sicurezza online",
+];
+
 export default function App() {
   return (
     <>
       <section>
-        <div className="container text-center">
-          <h1>CIAOOOO</h1>
-        </div>
+        <div className="container text-center"></div>
       </section>
     </>
   );
