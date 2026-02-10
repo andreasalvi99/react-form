6a8cebb07b013775666311035e1fd8ada36b8162
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-10 15:48:07 +0100

    deleting element added

--- src/App.jsx
+++ src/App.jsx
@@ -45,12 +45,34 @@ export default function App() {
         id="article-list"
         className="p-5 d-flex justify-content-center align-items-center flex-column"
       >
+        <h1 className="mb-3 fw-bolder">HEADLINES</h1>
         <div className="container text-start p-3 bg-primary-subtle rounded-3">
           <ul className="list-group list-group-flush rounded-3">
             {headLines.map((headLine, index) => {
               return (
-                <li key={index} className="list-group-item fw-bold p-3">
+                <li
+                  key={index}
+                  className="list-group-item fw-bold p-3 d-flex justify-content-between"
+                >
                   {headLine}
+                  <button>
+                    <i
+                      className="bi bi-trash-fill"
+                      onClick={() => {
+                        console.log("index:", index);
+
+                        const updatedHeadlines = headLines.filter(
+                          (_, newIndex) => {
+                            console.log("newIndex:", newIndex);
+
+                            return newIndex !== index;
+                          },
+                        );
+                        console.log(updatedHeadlines);
+                        setArticle(updatedHeadlines);
+                      }}
+                    ></i>
+                  </button>
                 </li>
               );
             })}
